c2dcb03272e164f9693963eb357f95a082eaa31d
AliEdalat <ali.edalat@ut.ac.ir>
2016-11-06 22:18:04 +0330

    design: completed,code is dirty

--- CA1-810195427.cpp
+++ CA1-810195427.cpp
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
+void show_result(int position, int color, int x, int y);
+int correct_colors(int random_number, int input);
+int correct_position(int* random_number, int *input);
+int genarate_random_number(void);
+void draw_circles(char input, int x, int y);
+bool correct_input(int input);
+void win_interface();
+void loss_interface(int random_number);
+void Game_background();
+
 int genarate_random_number(void) {
 	int i = 0;
 	int random_number = 0;
@@ -66,7 +77,7 @@ int correct_position(int* random_number, int *input) {
 	*input = inputdig1 + inputdig2 * 10 + inputdig3 * 100 + inputdig4 * 1000;
 	return position;
 }
-int correct_colors(int *random_number, int *input) {
+int correct_colors(int random_number, int input) {
 	int color = 0;
 	int random_numberdig1;
 	int random_numberdig2;
@@ -76,22 +87,24 @@ int correct_colors(int *random_number, int *input) {
 	int inputdig2;
 	int inputdig3;
 	int inputdig4;
-	random_numberdig1 = *random_number % 10;
-	*random_number /= 10;
-	random_numberdig2 = *random_number % 10;
-	*random_number /= 10;
-	random_numberdig3 = *random_number % 10;
-	*random_number /= 10;
-	random_numberdig4 = *random_number % 10;
-	*random_number /= 10;
-	inputdig1 = *input % 10;
-	*input /= 10;
-	inputdig2 = *input % 10;
-	*input /= 10;
-	inputdig3 = *input % 10;
-	*input /= 10;
-	inputdig4 = *input % 10;
-	*input /= 10;
+	
+	random_numberdig1 = random_number % 10;
+	random_number /= 10;
+	random_numberdig2 = random_number % 10;
+	random_number /= 10;
+	random_numberdig3 = random_number % 10;
+	random_number /= 10;
+	random_numberdig4 = random_number % 10;
+	random_number /= 10;
+	inputdig1 = input % 10;
+	input /= 10;
+	inputdig2 = input % 10;
+	input /= 10;
+	inputdig3 = input % 10;
+	input /= 10;
+	inputdig4 = input % 10;
+	input /= 10;
+	
 	if (inputdig1 == random_numberdig1&&inputdig1 != 0) {
 		color++;
 		inputdig1 = 0;
@@ -174,93 +187,77 @@ int correct_colors(int *random_number, int *input) {
 	}
 	return color;
 }
-void draw_result_circles(char kind, int x, int y);
+
 void show_result(int position,int color,int x,int y) {
 	int i = 0;
 	int s = 25;
 	for ( i = 0; i < position; i++)
 	{
-		draw_result_circles('p', x, y);
+		draw_circles('p', x, y);
 		x += s;
 	}
 	for ( i = 0; i < color; i++)
 	{
-		draw_result_circles('c', x, y);
+		draw_circles('c', x, y);
 		x += s;
 	}
 }
-void draw_result_circles(char kind, int x, int y) {
-	if (kind == 'p') {
-		setcolor(8);
-		circle(x, y, 10);
-		setfillstyle(SOLID_FILL, BLACK);
-		floodfill(x, y, 8);
-	}
-	if (kind == 'c') {
-		setcolor(8);
-		circle(x, y, 10);
-		setfillstyle(SOLID_FILL, WHITE);
-		floodfill(x, y, 8);
-	}
-}
-void draw_input_circles(int input,int x,int y) {
-	if (input == 1) {
-		setcolor(8);
+
+void draw_circles(char input,int x,int y) {
+	setcolor(BLACK);
+	if (input == '1') {
 		circle(x, y, 20);
 		setfillstyle(SOLID_FILL, WHITE);
-		floodfill(x, y, 8);
+		floodfill(x, y, BLACK);
 	}
-	if (input == 2) {
-		setcolor(8);
+	if (input == '2') {
 		circle(x, y, 20);
 		setfillstyle(SOLID_FILL, BLACK);
-		floodfill(x, y, 8);
+		floodfill(x, y, BLACK);
 	}
-	if (input == 3) {
-		setcolor(8);
+	if (input == '3') {
 		circle(x, y, 20);
 		setfillstyle(SOLID_FILL, BLUE);
-		floodfill(x, y, 8);
+		floodfill(x, y, BLACK);
 	}
-	if (input == 4) {
-		setcolor(8);
+	if (input == '4') {
 		circle(x, y, 20);
 		setfillstyle(SOLID_FILL, RED);
-		floodfill(x, y, 8);
+		floodfill(x, y, BLACK);
 	}
-	if (input == 5) {
-		setcolor(8);
+	if (input == '5') {
 		circle(x, y, 20);
 		setfillstyle(SOLID_FILL, YELLOW);
-		floodfill(x, y, 8);
+		floodfill(x, y, BLACK);
 	}
-	if (input == 6) {
-		setcolor(8);
+	if (input == '6') {
 		circle(x, y, 20);
 		setfillstyle(SOLID_FILL, GREEN);
-		floodfill(x, y, 8);
+		floodfill(x, y, BLACK);
+	}
+	if (input == 'p') {
+		circle(x, y, 10);
+		setfillstyle(SOLID_FILL, BLACK);
+		floodfill(x, y, BLACK);
+	}
+	if (input == 'c') {
+		circle(x, y, 10);
+		setfillstyle(SOLID_FILL, WHITE);
+		floodfill(x, y, BLACK);
 	}
 }
-void show_input(int input,int x,int y) {
-	int dig1=0;
-	int dig2=0;
-	int dig3=0;
-	int dig4=0;
-	int i = 0;
-	dig1 = input % 10;
-	input /= 10;
-	dig2 = input % 10;
-	input /= 10;
-	dig3 = input % 10;
-	input /= 10;
-	dig4 = input % 10;
-	input /= 10;
-	draw_input_circles(dig4,x,y);
-	draw_input_circles(dig3,x+50,y);
-	draw_input_circles(dig2,x+100,y);
-	draw_input_circles(dig1,x+150,y);
+void show_input(int input,int FirstCircleInLine_x,int FirstCircleInLine_y) {
+	int i ;
+	int FinalCircleInLine_X = FirstCircleInLine_x + 150;
+	int FinalCircleInLine_Y = FirstCircleInLine_y;
+	for (i = 1;i <= 4;i++) {
+		draw_circles(48 + (input % 10), FinalCircleInLine_X, FinalCircleInLine_Y);
+		FinalCircleInLine_X -= 50;
+		input /= 10;
+	}
+	
 }
-/*bool correct_input(int input) {
+bool correct_input(int input) {
 	bool correct = true;
 	int digits = 0;
 	while (input > 0) {
@@ -271,68 +268,84 @@ void show_input(int input,int x,int y) {
 		}
 		input /= 10;
 	}
-	if (digits > 4) {
+	if (digits > 4||digits<4) {
 		correct = false;
 	}
 	return correct;
-}*/
-bool EndGame(int position) {
-	bool endgame = false;
-	if (position == 4)
-		endgame = true;
-	return endgame;
+}
+void win_interface() {
+	cleardevice();
+	setcolor(BLACK);
+	outtextxy(200, 250, "You Won The Game");
+	printf("You Won The Game!\n");
+}
+void loss_interface(int random_number) {
+	printf("You  Loss!\n");
+	cleardevice();
+	show_input(random_number, 45, 45);
+	setcolor(BLACK);
+	outtextxy(200, 90, "You Loss");
+
+}
+void Game_background() {
+	initwindow(500, 600, "Master Mind");
+	setlinestyle(SOLID_LINE, 1, 2);
+	setbkcolor(DARKGRAY);
+	clearviewport();
+	line(300, 0, 300, 600);
 }
 int main() {
 	int wid = ALL_WINDOWS;
 	int input = 0;
 	int j = 45;
+	int position = 0;
+	int color = 0;
 	int random_number = 0;
 	bool end = false;
-	initwindow(500, 600, "MASTER MIND");
-	setlinestyle(SOLID_LINE, 1, 2);
-	setbkcolor(BROWN);
-	clearviewport();
-	line(300, 0, 300, 600);
+
+	Game_background();
 	random_number=genarate_random_number();
 	printf("Welcome To Master Mind!\n\nI have chosen a number. It Has 4 Digits and it is in range of[1111,6666].\n");
+	
 	for ( j = 45; j <545; j+=50)
 	{
-		int y = random_number;
+		int My_number = random_number;
 		printf("Guess my number:");
 		scanf("%d", &input);
-		printf("%d\n", y);
+		
+		while (!correct_input(input)) {
+			printf("your input is not correct!\nGuess my number: ");
+			scanf("%d", &input);
+		
+		}
+		
+		printf("%d\n", My_number);
 		show_input(input, 45, j);
-		int position = correct_position(&y, &input);
-		int color = correct_colors(&y, &input);
+		position = correct_position(&My_number, &input);
+		color = correct_colors(My_number, input);
 		printf("You guessed %d positions correctly!\nYou guessed %d colors correctly!\n\n", position, color);
 		show_result(position, color, 345, j);
-		if (EndGame(position)) {
-			setcolor(BLACK);
-			setlinestyle(SOLID_LINE, 2, 10);
-			settextstyle(BOLD_FONT, HORIZ_DIR, 20);
-			outtextxy(305, j+45, "You Won The Game");
-			printf("You Won The Game!\n");
+
+		/*this part show win condition and i use win_interface() to design interface. */
+		if (position==4) {	
+			win_interface();
 			end = true;
 			break;
 		}
 		
 	}
+	
+	/*this part show loss condition and i use "loss_interface" function to design interface. */
 	if (!end) {
-		printf("You Loss The Game!\n");
-		show_input(random_number, 45, 545);
-		setcolor(BLACK);
-		setlinestyle(SOLID_LINE, 2, 10);
-		settextstyle(BOLD_FONT, HORIZ_DIR, 20);
-		outtextxy(355,525, "My Colors");
-		setcolor(BLACK);
-		setlinestyle(SOLID_LINE, 2, 10);
-		settextstyle(BOLD_FONT, HORIZ_DIR, 20);
-		outtextxy(355, 545, "You Loss");
+		loss_interface(random_number);
+		
 	}
+	/*this part runs until you press close button.*/
 	while (!kbhit())
 	{
 		
 	}
+	
 	closegraph(wid);
 	return 0;
 }
